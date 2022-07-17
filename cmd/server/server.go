dd858d6bbf0a3ccfa1c7f10f2568a03cdd648bb2
Arthur Marques <arthur.l2@hotmail.com>
2022-07-17 01:45:45 -0300

    Implementando serviço

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,16 +1,24 @@
 package main
 
+import (
+	"github.com/arthur1470/fc2-grpc/pb"
+	"github.com/arthur1470/fc2-grpc/services"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
 func main() {
 	lis, err := net.Listen("tcp", "localhost:50051")
 
-	if err != null {
+	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
 
-	if err := grpcServer.Serve(lis); err != null {
+	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
 	}
-
 }
