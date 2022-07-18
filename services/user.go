6602d7082bc3bbc3be1635195bc0875b4a025f96
Arthur Marques <arthur.l2@hotmail.com>
2022-07-17 23:47:58 -0300

    Implementando server stream

--- services/user.go
+++ services/user.go
@@ -3,13 +3,17 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"github.com/arthur1470/fc2-grpc/pb"
 )
 
-type UserServiceServer interface {
-	AddUser(context.Context, *pb.User) (*pb.User, error)
-	mustEmbedUnimplementedUserServiceServer()
-}
+//
+//type UserServiceServer interface {
+//	AddUser(context.Context, *pb.User) (*pb.User, error)
+//	mustEmbedUnimplementedUserServiceServer()
+//	AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
+//}
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
@@ -29,4 +33,36 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
